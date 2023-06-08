32ce6e337bc1ab398432e879945effcef1de3427
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-08 22:30:07 +0700

    fix animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,8 @@ export default function Header() {
   const [show, setShow] = useState(false);
   const transition = useTransition(show, {
     from: { y: 50, opacity: 0 },
-    enter: { y: 0, opacity: 1 },
+    enter: { y: 0, x: 0, opacity: 1 },
+    leave: { y: 500 },
   });
 
   return (
@@ -15,56 +16,55 @@ export default function Header() {
           Vo Quang Chien
         </a>
 
-        {show &&
-          transition((style) => {
-            return (
-              <animated.div className="nav__menu" id="nav-menu" style={style}>
-                <ul className="nav__list">
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#home" className="nav__link">
-                      <i className="bx bx-home"></i>Home
-                    </a>
-                  </li>
+        {transition((style, item) => {
+          return item ? (
+            <animated.div className="nav__menu" id="nav-menu" style={style}>
+              <ul className="nav__list">
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#home" className="nav__link">
+                    <i className="bx bx-home"></i>Home
+                  </a>
+                </li>
 
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#profile" className="nav__link">
-                      <i className="bx bx-user"></i>Profile
-                    </a>
-                  </li>
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#profile" className="nav__link">
+                    <i className="bx bx-user"></i>Profile
+                  </a>
+                </li>
 
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#" className="nav__link">
-                      <i className="bx bxs-book"></i>Education
-                    </a>
-                  </li>
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#" className="nav__link">
+                    <i className="bx bxs-book"></i>Education
+                  </a>
+                </li>
 
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#" className="nav__link">
-                      <i className="bx bxs-receipt"></i>Skills
-                    </a>
-                  </li>
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#" className="nav__link">
+                    <i className="bx bxs-receipt"></i>Skills
+                  </a>
+                </li>
 
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#" className="nav__link">
-                      <i className="bx bxs-briefcase"></i>Experience
-                    </a>
-                  </li>
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#" className="nav__link">
+                    <i className="bx bxs-briefcase"></i>Experience
+                  </a>
+                </li>
 
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#" className="nav__link">
-                      <i className="bx bxs-award"></i>Certificates
-                    </a>
-                  </li>
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#" className="nav__link">
+                    <i className="bx bxs-award"></i>Certificates
+                  </a>
+                </li>
 
-                  <li className="nav__item" onClick={() => setShow(false)}>
-                    <a href="#" className="nav__link">
-                      <i className="bx bx-link-external"></i>References
-                    </a>
-                  </li>
-                </ul>
-              </animated.div>
-            );
-          })}
+                <li className="nav__item" onClick={() => setShow(false)}>
+                  <a href="#" className="nav__link">
+                    <i className="bx bx-link-external"></i>References
+                  </a>
+                </li>
+              </ul>
+            </animated.div>
+          ) : null;
+        })}
 
         <div className="nav__toggle" id="nav-toggle" onClick={() => setShow(!show)}>
           <i className="bx bxs-grid-alt"></i>
