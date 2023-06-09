c016e8921188a46cd116a33fbeae9ff2e2c7500e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-09 10:45:59 +0700

    add id for each section

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ export default function Profile() {
       <p className="profile__description">
         I'm always learning to improve my programming skills to develop professionally. I'm always work responsibly and
         happy to take on any task. I have several years of experience working with JavaScript, TypeScript, HTML & CSS,
-        React, Redux, React Spring, NEXT.js and Git.
+        React, Redux, React Spring, NEXT.js, Linux Terminal and Git.
       </p>
     </section>
   );
