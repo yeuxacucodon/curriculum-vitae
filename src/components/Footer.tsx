a130c8d337bdf50e12eb54e6966090a2e4d8ca96
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-14 21:24:40 +0700

    move to next js

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <section className="footer">
-      <p>
-        Made with <i className="bx bxs-heart"></i> by yeuxacucodon
-      </p>
-    </section>
-  );
-}
