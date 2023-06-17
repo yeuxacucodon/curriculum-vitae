3d6802d5478923dce2d5de580db1c0fc65668ded
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-17 13:40:22 +0700

    update

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,8 +7,8 @@ export default function Skills() {
       <h2 className="section-title">Skills</h2>
 
       <div className={[styles.skills__content, "bd-grid"].join(" ")}>
-        <SkillsList list={["HTML", "CSS, Sass", "Shell / Bash", "JavaScript", "TypeScript", "Rust", "Java"]} />
-        <SkillsList list={["React", "React Spring", "Redux / Zustand", "Tailwind CSS", "Firebase", "Git"]} />
+        <SkillsList list={["HTML, CSS", "Shell / Bash", "JavaScript", "TypeScript", "Rust", "Java"]} />
+        <SkillsList list={["React", "React Spring", "Redux / Zustand", "Tailwind CSS", "Sass", "Git"]} />
       </div>
     </section>
   );
