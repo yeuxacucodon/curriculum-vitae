561e1fb5abdab7bb888863e2a120dbc8094bcab6
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-20 08:47:59 +0700

    refactor code

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,19 @@
 import styles from "@/assets/sass/skills.module.scss";
-import SkillsList from "./SkillsList";
+
+function SkillsList({ list }: { list: string[] }) {
+  return (
+    <ul>
+      {list.map((skill, index) => {
+        return (
+          <li key={index} className={styles.skills__name}>
+            <span className={styles.skills__circle}></span>
+            {skill}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 export default function Skills() {
   return (
