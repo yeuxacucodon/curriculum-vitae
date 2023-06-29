46d0d934dc434825d304f48a6d8fbcb51bccf458
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-29 08:26:30 +0700

    update

--- src/content/Skills.tsx
+++ src/content/Skills.tsx
@@ -7,8 +7,8 @@ const SkillContent: React.FC = () => {
       <h2 className="section-title">Skills</h2>
       <div className={[styles.skills__content, "bd-grid"].join(" ")}>
         <Skills list={["HTML, CSS", "Shell", "JavaScript", "TypeScript", "Rust", "Java"]} />
-        <Skills list={["Redux / Zustand", "React", "React Spring", "Tailwind CSS", "Sass", "Express"]} />
-        <Skills list={["Docker", "Git"]} />
+        <Skills list={["Redux / Zustand", "React", "React Spring", "Tailwind CSS", "Express"]} />
+        <Skills list={["PostgreSQL", "Docker", "Git"]} />
       </div>
     </section>
   );
